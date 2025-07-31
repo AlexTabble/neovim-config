b3695e25b3467b6561f5a4ef4882140d89f09577
Alexander Leak <alexanderjleak@gmail.com>
2025-07-31 22:43:22 +0200

    Added latex support

--- lua/plugins/markdown/render-markdown.lua
+++ lua/plugins/markdown/render-markdown.lua
@@ -1,11 +1,12 @@
 return {
-    "MeanderingProgrammer/render-markdown.nvim",
-    dependencies = { "nvim-treesitter/nvim-treesitter", "nvim-tree/nvim-web-devicons" },
-    ops = {
-        on_attach = function(bufnr)
-            vim.schedule(function()
-                vim.schedule("MarkdownRender")
-            end)
-        end,
+    "MeanderingProgrammer/markdown.nvim",
+    name = "render-markdown",
+    ft = "markdown",
+    opts = {
+        headings = { "󰲡 ", "󰲣 ", "󰲥 ", "󰲧 ", "󰲩 ", "󰲫 " }, -- Nerd Font icons
+        bullets = { "●", "○", "◆", "◇" },
+    },
+    dependencies = {
+        "nvim-treesitter/nvim-treesitter",
     },
 }
