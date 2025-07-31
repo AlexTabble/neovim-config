9ce7f3db024f734890352ecafeacf95b9c2b1f6c
Alexander Leak <alexanderjleak@gmail.com>
2025-07-31 22:44:05 +0200

    Added latex support

--- lua/plugins/markdown/vimtex.lua
+++ lua/plugins/markdown/vimtex.lua
@@ -4,5 +4,9 @@ return {
     init = function()
         vim.g.vimtex_view_method = "zathura"
         vim.g.vimtex_syntax_conceal_disable = 1
+        vim.g.vimtex_compiler_method = "latexmk"
+        vim.g.vimtex_compiler_latexmk = {
+            continuous = 1,
+        }
     end,
 }
