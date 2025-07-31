479c4822b97e771ab219e2186d00324ded875bf4
Alexander Leak <alexanderjleak@gmail.com>
2025-07-31 22:44:42 +0200

    Better terminal support

--- lua/plugins/terminal/neogit.lua
+++ lua/plugins/terminal/neogit.lua
@@ -1,24 +1,8 @@
--- Not really neogit as I'm still learning git
 return {
-    "akinsho/toggleterm.nvim",
-    version = "*",
-    config = function()
-        require("toggleterm").setup({
-            direction = "float",
-            float_opts = {
-                border = "curved",
-            },
-        })
-
-        -- Optional: keymap to open Git terminal
-        vim.keymap.set("n", "<leader>g", function()
-            require("toggleterm.terminal").Terminal
-                :new({
-                    cmd = os.getenv("SHELL"),
-                    hidden = true,
-                    direction = "float",
-                })
-                :toggle()
-        end, { desc = "Open Git terminal" })
-    end,
+    "NeogitOrg/neogit",
+    dependencies = {
+        "nvim-lua/plenary.nvim",
+        "sindrets/diffview.nvim",
+        "nvim-telescope/telescope.nvim",
+    },
 }
