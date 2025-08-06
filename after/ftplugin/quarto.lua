c75175935b4c4add7f9867c364e6d9c9b157c34d
Alexander Leak <alexanderjleak@gmail.com>
2025-08-06 09:13:12 +0200

    Completed iron and quarto integration

--- after/ftplugin/quarto.lua
+++ after/ftplugin/quarto.lua
@@ -5,19 +5,16 @@ vim.keymap.set("n", "<leader>qij", "<Esc>O```{julia}<Esc>o```<Esc>O", { desc = "
 vim.keymap.set("n", "<leader>qic", "<Esc>O```<Esc>o```<Esc>O", { desc = "[I]nsert [C]ode chunk" })
 
 require("otter").activate()
-
--- Features from jmbuhr's Quarto plugin:
 require("quarto").activate()
+
+-- Preview stuff
 vim.keymap.set("n", "<leader>pQ", "<cmd>QuartoPreview<cr>", { desc = "[P]review" })
 vim.keymap.set("n", "<leader>qR", "<cmd>QuartoRender<cr>", { desc = "[R]ender" })
--- Set the plugin/keymaps up to provide slime with code to run
-local runner = require("quarto.runner")
-vim.keymap.set("n", "<localleader>qrc", runner.run_cell, { desc = "[r]un [c]ell", silent = true })
-vim.keymap.set("n", "<localleader>qra", runner.run_above, { desc = "[r]un cell and [a]bove", silent = true })
-vim.keymap.set("n", "<localleader>qrb", runner.run_below, { desc = "[r]un cell and [b]elow", silent = true })
-vim.keymap.set("n", "<localleader>qrA", runner.run_all, { desc = "[r]un [A]ll cells", silent = true })
-vim.keymap.set("n", "<localleader>qrl", runner.run_line, { desc = "[r]un [l]ine", silent = true })
-vim.keymap.set("v", "<localleader>qrr", runner.run_range, { desc = "run visual [r]ange", silent = true })
-vim.keymap.set("n", "<localleader>qRA", function()
-	runner.run_all(true)
-end, { desc = "[R]un [A]ll cells of all languages", silent = true })
+
+-- Syncs current repl instance which avoids using quarto.runner
+local keeper = require("otter.keeper")
+
+vim.keymap.set("n", "<localleader>tb", function()
+	lines = keeper.get_language_lines_around_cursor()
+	require("iron.core").send(nil, lines)
+end)
--- lua/plugins/iron.lua
+++ lua/plugins/iron.lua
@@ -3,7 +3,6 @@ return {
 	config = function()
 		local iron = require("iron.core")
 		local view = require("iron.view")
-		local common = require("iron.fts.common")
 
 		iron.setup({
 			config = {
@@ -15,24 +14,33 @@ return {
 					r = {
 						command = { "radian" },
 					},
+					quarto = {
+						command = { "ipython", "--no-autoindent" },
+						block_dividers = {
+							"```{python}",
+							"```{r}",
+							"```",
+						},
+					},
 				},
 				repl_open_cmd = view.split.vertical.botright("40%"),
 			},
+
 			keymaps = {
 				toggle_repl = "<leader>tr",
 				restart_repl = "<leader>tR",
-				send_motion = "<leader>tc",
+				-- send_motion = "<leader>tc",
 				visual_send = "<leader>tc",
 				send_file = "<leader>tf",
 				send_line = "<leader>tl",
 				send_paragraph = "<leader>tp",
 				send_until_cursor = "<leader>tu",
-				send_mark = "<leader>tm",
+				-- send_mark = "<leader>tm",
 				send_code_block = "<leader>tb",
-				send_code_block_and_move = "<leader>tn",
-				mark_motion = "<leader>tmc",
-				mark_visual = "<leader>tmc",
-				remove_mark = "<leader>tmd",
+				-- send_code_block_and_move = "<leader>tn",
+				-- mark_motion = "<leader>tmc",
+				-- mark_visual = "<leader>tmc",
+				-- remove_mark = "<leader>tmd",
 				cr = "<leader>t<cr>",
 				interrupt = "<leader>t<space>",
 				exit = "<leader>tq",
