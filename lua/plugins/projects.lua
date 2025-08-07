4955ee8623a8b48f07279516a5fbd4423e40c496
Alexander Leak <alexanderjleak@gmail.com>
2025-08-07 19:38:47 +0200

    add obsidian as project

--- lua/plugins/projects.lua
+++ lua/plugins/projects.lua
@@ -1,26 +1,27 @@
 return {
-    "coffebar/neovim-project",
-    opts = {
-        projects = { -- define project roots
-            "~/Projects/*",
-            "~/.config/nvim",
-        },
-        picker = {
-            type = "telescope", -- one of "telescope", "fzf-lua", or "snacks"
-        },
-        last_session_on_startup = false,
-    },
+	"coffebar/neovim-project",
+	opts = {
+		projects = { -- define project roots
+			"~/Projects/*",
+			"~/.config/nvim",
+			"~/Documents/ObVault",
+		},
+		picker = {
+			type = "telescope", -- one of "telescope", "fzf-lua", or "snacks"
+		},
+		last_session_on_startup = false,
+	},
 
-    init = function()
-        -- enable saving the state of plugins in the session
-        vim.opt.sessionoptions:append("globals") -- save global variables that start with an uppercase letter and contain at least one lowercase letter.
-    end,
-    dependencies = {
-        { "nvim-lua/plenary.nvim" },
-        { "nvim-telescope/telescope.nvim" },
+	init = function()
+		-- enable saving the state of plugins in the session
+		vim.opt.sessionoptions:append("globals") -- save global variables that start with an uppercase letter and contain at least one lowercase letter.
+	end,
+	dependencies = {
+		{ "nvim-lua/plenary.nvim" },
+		{ "nvim-telescope/telescope.nvim" },
 
-        { "Shatur/neovim-session-manager" },
-    },
-    lazy = false,
-    priority = 100,
+		{ "Shatur/neovim-session-manager" },
+	},
+	lazy = false,
+	priority = 100,
 }
