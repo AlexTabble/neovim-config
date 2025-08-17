728b0da466db73710f3f745ee33da030ee93cb6a
Alexander Leak <alexanderjleak@gmail.com>
2025-08-17 18:04:30 +0200

    TODO: Fix latex integration

--- lua/plugins/latex-integration.lua
+++ lua/plugins/latex-integration.lua
@@ -1,45 +1,46 @@
 return {
-    {
-        "lervag/vimtex",
-        ft = { "tex", "markdown" },
-        init = function()
-            vim.g.vimtex_view_method = "zathura"
-            vim.g.vimtex_syntax_conceal_disable = 1
-            vim.g.vimtex_compiler_method = "latexmk"
-            vim.g.vimtex_compiler_latexmk = {
-                continuous = 1,
-            }
-        end,
-    },
-    {
-        "vim-latex/vim-latex",
-        ft = { "markdown" },
-        init = function()
-            vim.g.tex_flavor = "latex"
-            vim.g.imap_leader = ";"
-            vim.g.tex_indent_items = 0
-            vim.g.latex_suite_menu = 0
-        end,
-    },
+	{
+		"vim-latex/vim-latex",
+		ft = { "markdown" },
+		lazy = true,
+		init = function()
+			vim.g.tex_flavor = "latex"
+			vim.g.imap_leader = ";"
+			vim.g.tex_indent_items = 0
+			vim.g.latex_suite_menu = 0
+		end,
+	},
 
-    {
-        "jbyuki/nabla.nvim",
-        ft = { "markdown", "tex" },
-        keys = {
-            {
-                "<leader>pe",
-                function()
-                    require("nabla").popup()
-                end,
-                desc = "Preview Math (popup)",
-            },
-            {
-                "<leader>pv",
-                function()
-                    require("nabla").enable_virt()
-                end,
-                desc = "Enable inline math",
-            },
-        },
-    },
+	{
+		"jbyuki/nabla.nvim",
+		lazy = true,
+		ft = { "markdown", "tex" },
+		keys = {
+			{
+				"<leader>pe",
+				function()
+					require("nabla").popup()
+				end,
+				desc = "Preview Math (popup)",
+			},
+			{
+				"<leader>pv",
+				function()
+					require("nabla").enable_virt()
+				end,
+				desc = "Enable inline math",
+			},
+		},
+	},
+	{
+		"iurimateus/luasnip-latex-snippets.nvim",
+		lazy = true,
+		dependencies = { "lervag/vimtex", "L3MON4D3/LuaSnip" },
+		config = function()
+			require("luasnip-latex-snippets").setup({
+				use_treesitter = true,
+				allow_on_markdown = true,
+			})
+		end,
+	},
 }
