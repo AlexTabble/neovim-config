16af2085753d3824ab092366468c357e793d3152
Alexander Leak <alexanderjleak@gmail.com>
2025-08-17 18:03:45 +0200

    some new themes

--- lua/plugins/themes.lua
+++ lua/plugins/themes.lua
@@ -2,6 +2,7 @@ return {
 	{
 
 		"navarasu/onedark.nvim",
+		lazy = true,
 		priority = 1000, -- make sure to load this before all the other start plugins
 		opts = {},
 	},
@@ -14,5 +15,35 @@ return {
 				style = "darker",
 			})
 		end,
+	},
+	{
+		"rebelot/kanagawa.nvim",
+		lazy = true,
+		priority = 1000,
+		config = function()
+			require("kanagawa").setup({})
+		end,
+	},
+	{
+		"everviolet/nvim",
+		name = "evergarden",
+		priority = 1000, -- Colorscheme plugin is loaded first before any other plugins
+		opts = {
+			theme = {
+				variant = "fall", -- 'winter'|'fall'|'spring'|'summer'
+				accent = "red",
+			},
+			editor = {
+				transparent_background = false,
+				sign = { color = "none" },
+				float = {
+					color = "mantle",
+					solid_border = false,
+				},
+				completion = {
+					color = "surface0",
+				},
+			},
+		},
 	},
 }
