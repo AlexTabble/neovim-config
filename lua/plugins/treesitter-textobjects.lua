16517b31b585f08689c7aad04e45f9dc5de29a76
Alexander Leak <alexanderjleak@gmail.com>
2025-08-18 12:10:41 +0200

    Implement textobject editing

---
+++ lua/plugins/treesitter-textobjects.lua
@@ -0,0 +1,43 @@
+return {
+	"nvim-treesitter/nvim-treesitter-textobjects",
+	lazy = true,
+	config = function()
+		require("nvim-treesitter.configs").setup({
+			textobjects = {
+				select = {
+					enable = true,
+					lookahead = false,
+					keymaps = {
+						-- You can use the capture groups defined in textobjects.scm
+						["af"] = "@function.outer",
+						["if"] = "@function.inner",
+						-- ["ac"] = "@class.outer",
+						-- ["ic"] = "@class.inner",
+						["ac"] = "@block.inner",
+						["ic"] = "@block.outer",
+					},
+				},
+				move = {
+					enable = true,
+					set_jumps = true, -- whether to set jumps in the jumplist
+					goto_next_start = {
+						["]m"] = "@function.outer",
+						["]]"] = "@class.inner",
+					},
+					goto_next_end = {
+						["]M"] = "@function.outer",
+						["]["] = "@class.outer",
+					},
+					goto_previous_start = {
+						["[m"] = "@function.outer",
+						["[["] = "@class.inner",
+					},
+					goto_previous_end = {
+						["[M"] = "@function.outer",
+						["[]"] = "@class.outer",
+					},
+				},
+			},
+		})
+	end,
+}
